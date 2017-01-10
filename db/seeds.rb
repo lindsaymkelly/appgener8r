120b339b2ef0c720b80483c62ca21680c62aa65a
lindsaymkelly <lindzlady@gmail.com>
2017-01-09 22:19:49 -0500

    Add seed data for popular apps and nouns

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+nouns = Noun.create([{ name: 'Cats' }, { name: 'Corgis' }, { name: 'Tacos' }, { name: 'Gamers' }, { name: 'Musicians' }, { name: 'Opera Singers' }, { name: 'Zubats' }, { name: 'Goat Lovers' }, { name: 'Vegans' }, { name: 'Audiophiles' }, { name: 'Engineers' }])
+
+popular_apps = PopularApp.create([{ name: 'Tinder'}, { name: 'Facebook'}, { name: 'Instagram'}, { name: 'Uber'}, { name: 'Waze'}, { name: 'Twitter'}, { name: 'Pinterest'}, { name: 'UrbanSpoon'}, { name: 'Netflix'}, { name: 'Spotify'}, ])
